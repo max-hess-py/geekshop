6a6d7f6932f14a8651399561fe8397593e0264a4
Max Hess <max.hess.py@gmail.com>
2023-08-26 18:28:45 +0500

    Added DRY for templates

--- mainapp/views.py
+++ mainapp/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request, 'mainapp/index.html')
+    context = {'title': 'GeekShop'}
+    return render(request, 'mainapp/index.html', context)
 
 
 def products(request):
-    return render(request, 'mainapp/products.html')
+    context = {'title': 'GeekShop - Каталог'}
+    return render(request, 'mainapp/products.html', context)
